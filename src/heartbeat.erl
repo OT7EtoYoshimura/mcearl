ab9d37cad653b8fc025c5365f0fb74336580264d
Emilia P. Stoyanova <e.stoyanova@student.fontys.nl>
2024-03-21 09:29:54 +0100

    Better typing information

--- src/heartbeat.erl
+++ src/heartbeat.erl
@@ -1,28 +1,33 @@
+-feature(maybe_expr, enable).
 -module(heartbeat).
--compile(export_all).
+-export([salt/0, format/4]).
 -define(MINUTE, 60*1000).
 
-apply_interval() ->
-	Salt = salt(),
-	Url = format("32", "Testing", Salt, "0"),
-	timer:apply_interval(?MINUTE, ?MODULE, ?FUNCTION_NAME, []).
+% apply_interval() ->
+% 	Salt = salt(),
+% 	Url = format("32", "Testing", Salt, "0"),
+% 	timer:apply_interval(?MINUTE, ?MODULE, ?FUNCTION_NAME, []).
 
 format(Max, Name, Salt, Users) ->
-	Url = uri_string:normalize(#{
-		scheme => "http",
-		host => "www.classicube.net",
-		path => "/heartbeat.jsp"
-	}),
-	Query = uri_string:compose_query([
-		{"port", "25565"},
-		{"max", Max},
-		{"name", Name},
-		{"public", "True"},
-		{"version", "7"},
-		{"salt", Salt},
-		{"users", Users}
-	]),
-	Url ++ "?" ++ Query.
+	maybe
+		Url ?= uri_string:normalize(#{
+			scheme => "http",
+			host => "www.classicube.net",
+			path => "/heartbeat.jsp"
+		}),
+		Query ?= uri_string:compose_query([
+			{"port", "25565"},
+			{"max", Max},
+			{"name", Name},
+			{"public", "True"},
+			{"version", "7"},
+			{"salt", Salt},
+			{"users", Users}
+		]),
+		Url ++ "?" ++ Query
+	else
+		{error, Reason, Cause} -> {Reason, Cause}
+	end.
 	% uri_string:resolve(Query, Url).
 
 salt() -> lists:sublist(binary_to_list(base62:encode(rand:bytes(64))), 16).
--- src/protocol.erl
+++ src/protocol.erl
@@ -1,85 +1,143 @@
 -module(protocol).
--compile(export_all).
+-export([parse/1, build/1]).
+-include_lib("datum/include/datum.hrl").
 
--type byte() :: 0 .. 255.
--type sbyte() :: -128 .. 127.
--type short() :: -32768 .. 32767.
--type nebin() :: nonempty_binary().
--type fbyte() :: nebin().
--type reason() :: distance | tile | clicking | lag.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Protocol Specific types %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% -type types() :: by() | sby() | fby() | sh() | fsh() | str() | byarr().
+% -type pkt_id()   :: id | set_block | pos_and_orient | msg | ping | lvl_init
+%                   | lvl_data | lvl_fin | spawn | pos_and_orient_up | pos_up
+%                   | orient_up | despawn | disconnect | user_type_up.
+
+-type by()    :: 0 .. 255.
+-type sby()   :: -128 .. 127.
+% -type fby()   :: {-4..3, 0..32}.
+-type sh()    :: -32768 .. 32767.
+-type fsh()   :: {-1024..1023, 0..32}.
+-type str()   :: string().
+-type byarr() :: binary().
+
+-type pkt_load() :: {id, str(), str(), boolean()}
+                  | {set_block,   sh(), sh(), sh(),         by()}
+                  | {set_block_m, sh(), sh(), sh(), mode(), by()}
+                  | {pos_and_orient, sby(), fsh(), fsh(), fsh(), by(), by()}
+                  | {msg, sby(), str()}
+                  | {ping} | {level_init}
+                  | {lvl_data, sh(), byarr(), by()}
+                  | {lvl_fin, sh(), sh(), sh()}
+                  | {spawn, sby(), str(), fsh(), fsh(), fsh(), by(), by()}
+                  | {despawn, sby()}
+                  | {disconnect, reason()}
+                  | {user_type_up, boolean()}.
 
 %%%%%%%%%%%%%%%%%%%%
 % Client -> Server %
 %%%%%%%%%%%%%%%%%%%%
--spec read(nebin()) -> tuple().
+-spec parse(binary()) -> datum:option(pkt_load()).
 
-read(<<16#00, 16#7, Name:64/binary, Key:64/binary, 16#0>>) ->
-	{Name, Key};
-read(<<16#05, X:16/signed, Y:16/signed, Z:16/signed, Mode, BlockType>>) ->
-	{X, Y, Z, Mode, BlockType};
-read(<<16#08, PlayerId/signed, X:8/binary-unit:2, Y:8/binary-unit:2, Z:8/binary-unit:2, Yaw:8, Pitch:8>>) ->
-	{PlayerId, X, Y, Z, Yaw, Pitch};
-read(<<16#0d, PlayerId/signed, Message:64/binary>>) ->
-	{PlayerId, Message}.
+parse(<<16#00, 16#7, Name:64/binary, Key:64/binary, IsOp>>) ->
+	TrimmedName = string:trim(binary_to_list(Name)),
+	TrimmedKey  = string:trim(binary_to_list(Key)),
+	{id, TrimmedName, TrimmedKey, toOp(IsOp)}; % IsOp is allways 16#00
+parse(<<16#05, X:16/signed, Y:16/signed, Z:16/signed, Mode, BlockType>>) ->
+	{set_block_m, mkSh(X), mkSh(Y), mkSh(Z), toMode(Mode), mkBy(BlockType)};
+parse(<<16#08, PlayerId/signed, XInt:11/signed, XFrac:5, YInt:11/signed, YFrac:5, ZInt:11/signed, ZFrac:5, Yaw:8, Pitch:8>>) ->
+	{pos_and_orient, mkSby(PlayerId), mkFsh(XInt, XFrac), mkFsh(YInt, YFrac), mkFsh(ZInt, ZFrac), mkBy(Yaw), mkBy(Pitch)};
+parse(<<16#0d, PlayerId/signed, Message:64/binary>>) ->
+	TrimmedMessage = string:trim(binary_to_list(Message)),
+	{msg, mkSby(PlayerId), TrimmedMessage};
+parse(_) -> undefined.
 
 %%%%%%%%%%%%%%%%%%%%
 % Server -> Client %
 %%%%%%%%%%%%%%%%%%%%
--spec serverId(string(), string(), boolean()) -> nebin().
--spec ping() -> nebin().
--spec levelInit() -> nebin().
--spec levelDataChunk(short(), nebin(), byte()) -> nebin().
--spec levelFinalise(short(), short(), short()) -> nebin().
--spec setBlock(short(), short(), short(), byte()) -> nebin().
--spec spawnPlayer(sbyte(), string(), fbyte(), fbyte(), fbyte(), byte(), byte()) -> nebin().
--spec setPosAndOrient(sbyte(), fbyte(), fbyte(), fbyte(), byte(), byte()) -> nebin().
--spec despawnPlayer(sbyte()) -> nebin().
--spec message(sbyte(), string()) -> nebin().
--spec disconnectPlayer(reason()) -> nebin().
--spec updateUserType(boolean()) -> nebin().
+-spec build(pkt_load()) -> datum:option(binary()).
 
-serverId(Name, MOTD, IsOp) ->
-	PaddedName = list_to_binary(string:pad(Name, 64)),
-	PaddedMOTD = list_to_binary(string:pad(MOTD, 64)),
-	PlayerType = mkOp(IsOp),
-	<<16#00, 16#07, PaddedName/binary, PaddedMOTD/binary, PlayerType>>.
-ping() -> <<16#01>>.
-levelInit() -> <<16#02>>.
-levelDataChunk(Length, Data, PercentComplete) ->
-	<<16#03, Length:16/signed, Data:1024/binary, PercentComplete:8>>.
-levelFinalise(XSize, YSize, ZSize) ->
-	<<16#04, XSize:16/signed, YSize:16/signed, ZSize:16/signed>>.
-setBlock(X, Y, Z, BlockType) ->
-	<<16#06, X:16/signed, Y:16/signed, Z:16/signed, BlockType:8>>.
-spawnPlayer(PlayerId, Name, X, Y, Z, Yaw, Pitch) ->
-	<<16#07, PlayerId/signed, X:8/binary-unit:2, Y:8/binary-unit:2,
-	  Z:8/binary-unit:2, Yaw:8, Pitch:8>>.
-setPosAndOrient(PlayerId, X, Y, Z, Yaw, Pitch) ->
-	<<16#08, PlayerId:8/signed, X:8/binary-unit:2, Y:8/binary-unit:2,
-	  Z:8/binary-unit:2, Yaw:8, Pitch:8>>.
-despawnPlayer(PlayerId) ->
-	<<16#0c, PlayerId:8/signed>>.
-message(PlayerId, Message) ->
-	PaddedMessage = list_to_binary(string:pad(Message, 64)),
-	<<16#0d, PlayerId:8/signed, PaddedMessage/binary>>.
-disconnectPlayer(Reason) ->
-	StringReason = mkReason(Reason),
-	PaddedReason = list_to_binary(string:pad(StringReason, 64)),
-	<<16#0e, PaddedReason/binary>>.
-updateUserType(IsOp) ->
-	PlayerType = mkOp(IsOp),
-	<<16#0f, PlayerType>>.
+build({id, Name, MOTD, IsOp}) ->
+	PaddedName = pad(Name),
+	PaddedMOTD = pad(MOTD),
+	PlayerType = fromOp(IsOp),
+	<<16#00, 16#07, PaddedName/binary, PaddedMOTD/binary, PlayerType>>;
+build({ping}) -> <<16#01>>;
+build({level_init}) -> <<16#02>>;
+build({lvl_data, Length, Data, PercComp}) ->
+	<<16#03, Length:16/signed, Data:1024/binary, PercComp:8>>;
+build({lvl_fin, XSize, YSize, ZSize}) ->
+	<<16#04, XSize:16/signed, YSize:16/signed, ZSize:16/signed>>;
+build({set_block, X, Y, Z, BlockType}) ->
+	<<16#06, X:16/signed, Y:16/signed, Z:16/signed, BlockType:8>>;
+build({spawn, PlayerId, Name, {XInt, XFrac}, {YInt, YFrac}, {ZInt, ZFrac}, Yaw, Pitch}) ->
+	BinName = ch2bin((string:trim(Name))),
+	<<16#07, PlayerId/signed, BinName/binary,
+	  XInt:11/signed, XFrac:5, YInt:11/signed, YFrac:5, ZInt:11/signed, ZFrac:5,
+	  Yaw:8, Pitch:8>>;
+build({pos_and_orient, PlayerId, {XInt, XFrac}, {YInt, YFrac}, {ZInt, ZFrac}, Yaw, Pitch}) ->
+	<<16#08, PlayerId:8/signed,
+	  XInt:11/signed, XFrac:5, YInt:11/signed, YFrac:5, ZInt:11/signed, ZFrac:5,
+	  Yaw:8, Pitch:8>>;
+build({despawn, PlayerId}) ->
+	<<16#0c, PlayerId:8/signed>>;
+build({msg, PlayerId, Message}) ->
+	PaddedMessage = pad(Message),
+	<<16#0d, PlayerId:8/signed, PaddedMessage/binary>>;
+build({disconnect, Reason}) ->
+	StringReason = fromReason(Reason),
+	PaddedReason = pad(StringReason),
+	<<16#0e, PaddedReason/binary>>;
+build({user_type_up, IsOp}) ->
+	PlayerType = fromOp(IsOp),
+	<<16#0f, PlayerType>>;
+build(_) -> undefined.
 
 %%%%%%%%%%%%%
 % Utilities %
 %%%%%%%%%%%%%
--spec mkReason(reason()) -> string().
--spec mkOp(boolean()) -> char().
+-type reason() :: distance | tile | clicking | lag.
+-type mode()   :: created | destroyed.
 
-mkReason(distance) -> "Cheat detected: Distance";
-mkReason(tile)     -> "Cheat detected: Tile type";
-mkReason(clicking) -> "Cheat detected: Too much clicking!";
-mkReason(lag)      -> "Cheat detected: Too much lag".
+-spec fromReason(reason()) -> string().
+fromReason(distance) -> "Cheat detected: Distance";
+fromReason(tile)     -> "Cheat detected: Tile type";
+fromReason(clicking) -> "Cheat detected: Too much clicking!";
+fromReason(lag)      -> "Cheat detected: Too much lag".
 
-mkOp(true)  -> 16#64;
-mkOp(false) -> 16#0.
+-spec fromOp(boolean()) -> char().
+fromOp(true)  -> 16#64;
+fromOp(false) -> 16#00.
+-spec toOp(16#64 | 16#00) -> boolean().
+toOp(16#64) -> true;
+toOp(16#00) -> false.
+
+% fromMode(created)   -> 16#01;
+% fromMode(destroyed) -> 16#00.
+
+toMode(16#01) -> created;
+toMode(16#00) -> destroyed;
+toMode(_)     -> undefined.
+
+-spec mkSh(integer()) -> sh().
+-spec mkBy(integer()) -> by().
+-spec mkSby(integer()) -> sby().
+% -spec mkFby(integer(), integer()) -> fby().
+-spec mkFsh(integer(), integer()) -> fsh().
+
+mkSh(X) -> clamp(X, -32768, 32767).
+mkBy(X) -> clamp(X, 0, 255).
+mkSby(X) -> clamp(X, -128, 127).
+% mkFby(Integral, Frac) -> {clamp(Integral,    -4,   3), clamp(Frac, 0, 32)}.
+mkFsh(Integral, Frac) -> {clamp(Integral, -1024,1023), clamp(Frac, 0, 32)}.
+
+-spec pad(str()) -> binary().
+-spec ch2bin(unicode:chardata()) -> binary().
+pad(Str) -> ch2bin(string:pad(Str, 64)).
+ch2bin(Str) ->
+	case unicode:characters_to_binary(Str) of
+		{error, Encoded, _Rest} -> Encoded;
+		{incomplete, Encoded, _Rest} -> Encoded;
+		Binary -> Binary
+	end.
+
+clamp(X, Min, Max) -> min(max(X, Min), Max).
+
